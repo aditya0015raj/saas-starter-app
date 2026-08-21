18f55796bcea8872d587ab40e2c7bb1d0358a466
Aditya raj <aditya0015raj@gmail.com>
2026-08-21 19:20:04 +0530

    Update Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ["img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
   },
 };
 
